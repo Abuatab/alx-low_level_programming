73d8ec7851cf1c810b6a5d9392bbf3ce6f9bee7d
Amir clearuad <amir1006375@gmail.com>
2022-12-11 00:18:16 -0800

    going on

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * Description: the programs description
+ *
+ * main - Entry
+ *
+ * return: Always 0
+ */
 
-int main(void)
+int main(main)
 {
-puts("Programming is like building a multilingual puzzle\n")
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
