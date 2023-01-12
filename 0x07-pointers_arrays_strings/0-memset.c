5c217910b3d015588f6fe511c7f15d8a6486d155
Amir Murad <amir1006375@gmail.com>
2023-01-12 02:59:38 -0800

    blank

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,19 @@
 #include "main.h"
-/**
- *_memset - fills memory with a constant byte.
- *fills the first n bytes of the memory area pointed
- *to by s with the constant b.
- *@s: pointer to memory area.
- *@b: constant to fill memory with.
- *@n: bytes of the memory area to be filled.
- *
- *Return: pointer to memory area s.
-*/
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int counter;
 
-	for (counter = 0; counter < n; counter++)
+/**
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 if successful
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
 	{
-		s[counter] = b;
+		return (1);
 	}
-	return (s);
+	else
+	{
+		return (0);
+	}
 }
