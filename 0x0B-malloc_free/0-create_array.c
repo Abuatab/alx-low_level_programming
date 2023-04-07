af43d0fd0e941342057a50084c00bdfc33faeebe
Amir Murad <amir1006375@gmail.com>
2023-04-07 08:01:43 -0700

    task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,23 @@
 #include "main.h"
+/**
+ * create_array - creates an array dynamically
+ * @size: size of the array
+ * @c: the charcter filling the array
+ * Return: a pointer to an array of char
+ */
 
-int main()
+char *create_array(unsigned int size, char c)
+
 {
-	char s[10] = "Hello";
-	printf("%ld", sizeof(int));
-	_putchar('\n');
+	char *s = (char *) malloc(size * sizeof(int));
 
-	return (0);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (int i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
 }
