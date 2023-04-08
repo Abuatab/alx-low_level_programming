cce8b0c8c1f71ccc3878db3f908835e5060634ef
Amir Murad <amir1006375@gmail.com>
2023-04-08 09:45:06 -0700

    2nd task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strlen - counts the number of elements in a string
+ *@str: the string or to be counted
+ *Return: the number of elements
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
+
+/**
+ *_strdup - duplicates a string
+ *@str: the string to be duplicated
+ *Return: a pointer the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *s = (char *) malloc(_strlen(str) * sizeof(int));
+
+	if (s == NULL || _strlen(str) == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= _strlen(str); i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
