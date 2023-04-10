3e36f54c13a4e9f7d50e68e43a81fa9e10db019a
Amir Murad <amirmurad696@gmail.com>
2023-04-10 22:14:05 +0300

    3rd task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,70 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strlen - counts the number of elements in a string
+ *@str: the string or to be counted
+ *Return: the number of elements
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
+
+/**
+ *str_concat - duplicates a string
+ *@s1: The 1st string
+ *@s2: The 2nd string
+ *Return: a pointer the duplicated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, l;
+
+	j = _strlen(s1);
+	k = _strlen(s2);
+	char *s = (char *) malloc((j + k) * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < j; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (i = 0; i < k + j; i++)
+	{
+		s[j + i] = s2[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *s;
+
+	s = str_concat("Betty ", "Holberton");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
