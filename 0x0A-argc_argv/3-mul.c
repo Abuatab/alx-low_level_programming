f1b1cfceaa886097c0490f43dcfa996643d7a235
Amir Murad <amirmurad696@gmail.com>
2023-06-11 17:37:15 +0300

    Done with the fourth task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments
+ * @argc: argument counter
+ * @argv: string of arguments
+ * Return: Always 0(Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
