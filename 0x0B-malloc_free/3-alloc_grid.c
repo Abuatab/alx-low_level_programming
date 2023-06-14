5f1672f6d8a318670d712c9e28c4519792441acf
Amir Murad <amirmurad696@gmail.com>
2023-06-14 22:30:28 +0300

    fixed memory leak

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,6 +27,12 @@ int **alloc_grid(int width, int height)
 		p[i] = malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
+			for (i = 0; i < height; i++)
+			{
+				free(p[i]);
+			}
+			free(p);
+
 			return (NULL);
 		}
 	}
