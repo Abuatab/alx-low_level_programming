e29a6a1cfe1d3f8ef4252d3f90a61d90d5f242ec
Amir Murad <amirmurad696@gmail.com>
2023-06-16 19:31:46 +0300

    Fixed buffer overflow in malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	p = malloc(sizeof(int) * (max - min));
+	p = malloc(sizeof(int) * (max - min + 1));
 	if (p == NULL)
 	{
 		return (NULL);
