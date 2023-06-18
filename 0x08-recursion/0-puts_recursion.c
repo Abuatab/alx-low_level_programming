73ab97f1b091ec176a67010a822493b292fa4c92
Amir Murad <amirmurad696@gmail.com>
2023-06-18 12:31:46 +0300

    Done with the first task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _puts_recursion - Outputs a string recursively
+ * @s: The string to output
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
