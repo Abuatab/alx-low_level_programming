f9cf859d4b2e605e77505ae7a072e4641ecdbbfb
Amir Murad <amirmurad696@gmail.com>
2023-06-21 00:43:25 +0300

    A fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,6 @@
 #define DOG_H
 
 /**
- * struct dog - A profile for dogs
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- * Description: A struct for a dogs name, age and owner
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-/**
  * struct dog_t - A profile for dogs
  * @name: The name of the dog
  * @age: The age of the dog
