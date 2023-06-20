a038963617806734fe8320b7ed8279757c86a5eb
Amir Murad <amirmurad696@gmail.com>
2023-06-20 23:43:06 +0300

    Done with the first task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Project to learn about Structs and typdef in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A profile for dogs
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * Description: A struct for a dogs name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
