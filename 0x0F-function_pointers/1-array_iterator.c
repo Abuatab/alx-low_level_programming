327ffbb3bbeb3fede538348d11717c97b4cbd3f9
Amir Murad <amirmurad696@gmail.com>
2023-06-23 17:02:43 +0300

    Removed = from i <= size in the for loop which was causing it to go out of bounds

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
  * array_iterator - Executes action on the given array
  * @array: The array to execute action on
  * @action: A pointer to a function with an int parameter
- *@size: The size of the array
+ * @size: The size of the array
  * Return: Nothing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
