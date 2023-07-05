3e764523c63e26c1a3a8a56b4d606799d1e531d7
Amir Murad <amirmurad696@gmail.com>
2023-07-05 21:15:42 +0300

    Done with the third task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints the name, age, and owner of a dog
+ * @d: pointer to a struct dog
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+}
