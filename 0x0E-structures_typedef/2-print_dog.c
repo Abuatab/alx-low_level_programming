90effdf889497f4a574b7e3809f4b8476003262b
Amir Murad <amirmurad696@gmail.com>
2023-07-05 21:28:16 +0300

    I went back to the old style of checking if d is NULL since it's not changing the checkers output

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,20 +8,22 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == NULL)
 	{
-		if (d->name == NULL)
-		{
-			printf("Name: (nil)");
-		}
-		else
-			printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-			printf("Owner: %s\n", d->owner);
+		return;
+
 	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+		printf("Owner: %s\n", d->owner);
 }
