c979b1d5008f22b7938bf1e0f3e020f6af13463d
Amir <amirmurad696@gmail.com>
2024-03-17 22:10:04 +0300

    Fixed the length of the string

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,11 +11,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr;
+	size_t str_len = 0;
 
 	ptr = malloc(sizeof(list_t));
 	if (ptr == NULL)
 		return (NULL);
+
+	while (str[str_len])
+		str_len++;
 	ptr->str = strdup(str);
+	ptr->len = str_len;
 	ptr->next = *head;
 	*head = ptr;
 	return (*head);
