ee1093ea05ba7b3f393cbdc50cbd3b42032fc00a
Amir <amirmurad696@gmail.com>
2024-03-18 18:16:39 +0300

    return when head is NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (*head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
+	*head = NULL;
 }
