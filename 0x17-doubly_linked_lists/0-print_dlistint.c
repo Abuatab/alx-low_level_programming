5f10d221972aba2d6b030cad5a244079280ed729
Amir <amirmurad696@gmail.com>
2024-03-23 11:46:19 +0300

    Fixed betty issues

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
+
 	while (h != NULL)
 	{
 		counter++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return counter;
+	return (counter);
 }
