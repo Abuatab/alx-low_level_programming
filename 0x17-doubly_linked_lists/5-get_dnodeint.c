02bd13cb9a71541744d4ffbbb4eaccb199bd3d83
Amir <amirmurad696@gmail.com>
2024-03-23 17:02:13 +0300

    changed a wrong function name

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - gets a node at an index
+ * get_dnodeint_at_index - gets a node at an index
  * @head: the list to get the node from
  * @index: the index of the node
  * Return: a pointer to the node at index
  */
 
-dlistint_t *get_nodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *traverser;
 
